ea1599d4defbc5a79e0835d0f5f912cba4ffe9b5
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-14 18:52:32 -0400

    Process user interactions|answers

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,7 @@ class _QuizPageState extends State<QuizPage> {
     'Approximately one quarter of human bones are in the feet.',
     'A slug\'s blood is green.'
   ];
+  List<bool> answers = [false, true, true];
   int numQuestion = 0;
   @override
   Widget build(BuildContext context) {
@@ -70,6 +71,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                bool correctAnswer = answers[numQuestion];
+                if (correctAnswer == true) {
+                  print('correct');
+                } else {
+                  print('incorrect');
+                }
                 setState(() {
                   numQuestion++;
                 });
@@ -91,6 +98,12 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                bool correctAnswer = answers[numQuestion];
+                if (correctAnswer == false) {
+                  print('correct');
+                } else {
+                  print('incorrect');
+                }
                 setState(() {
                   numQuestion++;
                 });
